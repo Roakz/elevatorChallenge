131e51751059bfea8b7942b7cfe77a72460da83d
Rory B <rory_bell_88@hotmail.com>
2024-06-21 12:08:23 +1000

    Job class with fields, properties & constructor. Passenger class with fields, properties & constructor.

--- Elevator/Elevator/Program.cs
+++ Elevator/Elevator/Program.cs
@@ -8,6 +8,45 @@ namespace Elevator
 {
     class Program
     {
+
+        class Job
+        {
+            private int jobNumber;
+            internal byte Priority { get; set; }
+            internal  List<Passenger> Passengers  {get; set; }
+            public byte LiftAllocation { get; private set; }
+            internal List<int> pickUps = new List<int> { };
+            internal List<int> dropOffs = new List<int> { };
+
+            public Job(int jNumb, byte priority, List<Passenger>passengers, byte liftAllocation, int[] passPicks, int[] passDrops )
+            {
+                jobNumber = jNumb;
+                Priority = priority;
+                Passengers = passengers;
+                LiftAllocation = liftAllocation;
+                foreach (int p in passPicks) { pickUps.Add(p); }
+                foreach (int d in passDrops) { dropOffs.Add(d); }
+            }
+
+        }
+
+        class Passenger
+        {
+            internal string Name { get; private set; }
+            private byte pickUp;
+            private byte dropOff;
+            internal string CurrentLocation { get; private set; }
+ 
+            
+            public  Passenger(string name, byte pUp, byte dOff, string level)
+            {
+                Name = name;
+                pickUp = pUp;
+                dropOff = dOff;
+                CurrentLocation = level;
+            }
+
+        }
         static void Main(string[] args)
         {
         }
